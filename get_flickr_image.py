6f0860567a81b56c34eafd7485cd8f40249bcb48
Eunsong <eunsong.choi@gmail.com>
2014-10-23 02:39:43 -0400

    request links to image source

--- get_flickr_image.py
+++ get_flickr_image.py
@@ -2,11 +2,16 @@ import urllib.request
 import re
 
 searchTerm = 'dog'
-search_url = 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=' + searchTerm
+numItems = 50 # number of images to be requested 
+page = 10 # page number
+search_url = 'https://www.flickr.com/search/?q=' + searchTerm +\
+             '&per_page=' + str(numItems) + '&page=' + str(page)
 request = urllib.request.Request(search_url)
 response = urllib.request.urlopen(request).read().decode()
-image_srcs = re.findall(r'media\"\: \{\"m\"\:\"(.+?)\"', response)
+image_srcs = re.findall(r'data\-defer\-src\=\"(.+?)\"', response)
+image_srcs = list( filter( lambda x: 'https' in x, image_srcs)) # remove something like {{src}} 
 print(image_srcs) 
+print(len(image_srcs))
 
 #print(response)
 #with open('test.txt', 'w') as f:
